088570b96ccad4c6d9a190075964c5dc916a3235
Ven Gallardo <letskeddotcom@gmail.com>
2022-02-09 23:20:15 +0800

    async interval console log

--- index.js
+++ index.js
@@ -1,22 +1,72 @@
-let number = 0
-let initialTime = 0
-let firstTime = new Date()
+let usersFinished = 0
+const dateOne = new Date()
 
+function user1(){
+    loop()
+}
+function user2(){
+    loop2()
+}
+function user3(){
+    loop3()
+}
 
-for (i = 0; i < 100000; i++) {
-    number = number + 1
-    if(number === 100000){
-        initial()
-    }else{
-        console.log(number)
+function loop(){
+    let number = 0
+    let firstTime = new Date()
+
+    for (i = 0; i < 1000; i++) {
+        number = number + 1
+        if(number === 1000){
+            usersFinished +=1
+            initial(firstTime)
+        }else{
+            console.log(number)
+        }
+    }
+}
+function loop2(){
+    let number = 0
+
+    let firstTime = new Date()
+
+    for (i = 0; i < 1000; i++) {
+        number = number + 1
+        if(number === 1000){
+            usersFinished += 1
+            initial(firstTime)
+        }else{
+            console.log(number)
+        }
+    }
+}
+function loop3(){
+    let number = 0
+    let firstTime = new Date()
+
+    for (i = 0; i < 1000; i++) {
+        number = number + 1
+        if(number === 1000){
+            usersFinished += 1
+            initial(firstTime)
+        }else{
+            console.log(number)
+        }
     }
 }
 
-function initial(){
-    console.log(`Time started : ${firstTime}`)
-    const newTime = new Date()
-    console.log(`Time ended : ${newTime}`)
-    const subtractedTime = newTime - firstTime
+function initial({e}){
+    if(usersFinished === 3){
+        const newTime = new Date()
+        const subtractedTime = newTime - dateOne
+        console.log(`Total Time: ${subtractedTime} millisecond/s`)
 
-    console.log(`Total Time: ${subtractedTime} millisecond/s`)
+    }else{
+        console.log(`ended in Time: ${e} millisecond/s`)   
+    }
+    
 }
+
+user1()
+user2()
+user3()
